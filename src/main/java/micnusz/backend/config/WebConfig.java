3b15654c7e7a08581a5c96fd1530d6c4700ad67f
Michał <michal@MacBook-Pro-Micha.local>
2025-09-10 15:22:55 +0200

    Edited ConfigFiles

--- src/main/java/micnusz/backend/config/WebConfig.java
+++ src/main/java/micnusz/backend/config/WebConfig.java
@@ -1,5 +1,6 @@
 package micnusz.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @Configuration
 public class WebConfig implements WebFluxConfigurer {
 
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
